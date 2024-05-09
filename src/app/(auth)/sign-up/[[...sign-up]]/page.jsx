9342df8ae6c622c3180af98f6985bbd8859d1013
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-05-09 14:50:12 +0500

    Quick Fixes

--- src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,7 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
-    <section className="bg-[#F1F5F9]">
+    <section className="bg-[#F1F5F9] min-h-[100vh]">
       <div className="">
         <main className="flex items-center justify-center p-10">
           <SignUp signUpForcedRedirect />
--- src/app/_components/pageSections/Hero.jsx
+++ src/app/_components/pageSections/Hero.jsx
@@ -1,10 +1,13 @@
+"use client";
+
 import { Button } from "../../../components/ui/button";
+import { useRouter } from "next/navigation";
 
 const Hero = () => {
   return (
     <div>
-      <section className="bg=[#F1F5F9]">
-        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+      <section className=" bg=[#F1F5F9]">
+        <div className="mx-auto max-w-screen-xl px-4 py-20 lg:flex lg:h-screen lg:items-center">
           <div className="mx-auto max-w-xl text-center">
             <h1 className="text-3xl font-extrabold text-[#283841] sm:text-5xl">
               Track Every Penny
@@ -15,13 +18,14 @@ const Hero = () => {
 
             <p className="mt-4 sm:text-xl/relaxed">
               Our budget tracking app empowers you to take control of your
-              finances effortlessly. Say goodbye to financial stress and hello
-              to financial freedom with our intuitive interface that lets you
-              monitor every penny you spend and save.
+              finances effortlessly. Our app lets you monitor every penny you
+              spend and save.
             </p>
 
             <div className="mt-8 flex flex-wrap justify-center gap-4">
-              <Button>Get Started</Button>
+              <Button onClick={() => router.push("/signup")}>
+                Get Started
+              </Button>
               <Button variant="ghost">Learn More</Button>
             </div>
           </div>
