cc6ee9ae0bde8ec907f18288da1f37fb54efd152
Devmax_ <muhammadhamzashahid31@gmail.com>
2024-06-27 11:00:27 +0500

    fix:/Quick Fixes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,7 @@ import Header from "./_components/shared/Header";
 
 const Home = () => {
   return (
-    <div className="bg-[#F1F5F9]">
+    <div className="bg-white md:bg-[#F1F5F9]">
       <Header />
       <Hero />
     </div>
